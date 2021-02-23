230a07656d7a5d0e9c0704af730709fc8a2bca7c
ashish.behera <abehera2@sapient.com>
2021-02-24 00:19:51 +0530

    Headless Mode added

--- src/main/java/pratice/base.java
+++ src/main/java/pratice/base.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -30,13 +31,15 @@ public class base {
 		String browserName = System.getProperty("browser");
 		System.out.println("browserName"+browserName);
 		
-		if(browserName.equalsIgnoreCase("chrome")) {
-			System.out.println("I am Here");
-			System.setProperty("webdriver.chrome.driver", "//Users//ashbeher//Desktop//Personal//InterviewPrep//Automation//SeleniumLib//chromedriver");
-			 driver = new ChromeDriver();
+		if(browserName.contains("chrome")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +"//driver//chromedriver");
+			ChromeOptions options = new ChromeOptions();
+			if(browserName.contains("headless"))
+			options.addArguments("headless");
+			 driver = new ChromeDriver(options);
 		} 
 		else if(browserName.equalsIgnoreCase("firefox")) {		
-			System.setProperty("webdriver.gecko.driver", "//Users//ashbeher//Desktop//Personal//InterviewPrep//Automation//SeleniumLib//geckodriver");
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") +"//driver//geckodriver");
 			 driver = new FirefoxDriver();
 		}else if(browserName.equalsIgnoreCase("safari")) {			
 			 driver = new SafariDriver();
